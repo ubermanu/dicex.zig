04d48f097b10c17156e79b78aba09ff1e9667165
ubermanu <manu@paca.happy-dev.fr>
2025-01-07 00:15:31 +0100

    chore: reorganize files

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,6 @@
 pub const Dicex = @import("Dicex.zig");
-
 const Parser = @import("Parser.zig");
+
 pub const Dice = Parser.Dice;
 pub const Die = Parser.Die;
 
@@ -12,3 +12,8 @@ pub const d10 = Die{ .faces = 10 };
 pub const d12 = Die{ .faces = 12 };
 pub const d20 = Die{ .faces = 20 };
 pub const d100 = Die{ .faces = 100 };
+
+test {
+    _ = Dicex;
+    _ = Parser;
+}
